d7fed3a9b7804692e2c6ff46ba31608c37b98205
v.mosenkov <v.mosenkov@solarl.ru>
2020-11-09 11:39:02 +0300

    Поправил реквест создания контрактов

--- requests/contract_create_request.go
+++ requests/contract_create_request.go
@@ -1,12 +1,17 @@
 package requests
 
-import "github.com/SolarLabRU/fastpay-go-commons/enums/currency-exchange-contracts-category-enum"
+import (
+	"github.com/SolarLabRU/fastpay-go-commons/enums/currency-exchange-contracts-category-enum"
+	"github.com/SolarLabRU/fastpay-go-commons/models"
+)
 
 type ContractCreateRequest struct {
 	Id                   string                                                                      `json:"id" valid:"required"`
 	AddressAccountSell   string                                                                      `json:"addressAccountSell" valid:"validHex40~ErrorAddressNotFollowingRegex"`
 	AddressAccountBuy    string                                                                      `json:"addressAccountBuy" valid:"validHex40~ErrorAddressNotFollowingRegex"`
 	AddressCommission    string                                                                      `json:"addressCommission" valid:"validHex40~ErrorAddressNotFollowingRegex"`
+	CurrencyInfoSell     models.CurrencyInfo                                                         `json:"currencyInfoSell"`
+	CurrencyInfoBuy      models.CurrencyInfo                                                         `json:"currencyInfoBuy"`
 	Category             currency_exchange_contracts_category_enum.CurrencyExchangeContractsCategory `json:"category"`
 	Price                float64                                                                     `json:"price" valid:"required~ErrorAmountNotPassed,range(0|9223372036854775807)"`
 	FractionalCommission float64                                                                     `json:"fractionalCommission" valid:"range(0|1)"`

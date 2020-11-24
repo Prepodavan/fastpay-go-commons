8be88a9843a86fd78eb22eee1467721c58b92992
v.mosenkov <v.mosenkov@solarl.ru>
2020-11-24 17:24:13 +0300

    Добавил msgHash для UpdateAvailableContractTypesRequest

--- requests/update_available_contract_types_request.go
+++ requests/update_available_contract_types_request.go
@@ -1,9 +1,23 @@
 package requests
 
-import "github.com/SolarLabRU/fastpay-go-commons/enums/currency-exchange-contract-type-enum"
+import (
+	"fmt"
+	"github.com/SolarLabRU/fastpay-go-commons/enums/currency-exchange-contract-type-enum"
+)
 
 type UpdateAvailableContractTypes struct {
 	TechnicalSignRequest
 	Address                string                                                              `json:"address" valid:"required~ErrorAddressNotPassed,validHex40~ErrorAddressNotFollowingRegex"`
 	AvailableContractTypes []currency_exchange_contract_type_enum.CurrencyExchangeContractType `json:"availableContractTypes"`
+}
+
+func (request *UpdateAvailableContractTypes) String() string {
+
+	concatenatedTypes := ""
+
+	for _, v := range request.AvailableContractTypes {
+		concatenatedTypes += string(int(v))
+	}
+
+	return fmt.Sprintf("updateAvailableContractTypes%s%s", request.Address, concatenatedTypes)
 }

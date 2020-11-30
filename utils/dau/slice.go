649b19df23e01838d04a270e5ea6860416980bbe
veselisk <kars-v@bk.ru>
2020-11-30 13:26:02 +0500

    пагинация

--- utils/dau/slice.go
+++ utils/dau/slice.go
@@ -5,10 +5,11 @@ import (
 	"encoding/json"
 	ccErrors "github.com/SolarLabRU/fastpay-go-commons/cc-errors"
 	"github.com/hyperledger/fabric-chaincode-go/shim"
+	"github.com/hyperledger/fabric-protos-go/peer"
 )
 
-func FindAll(stub shim.ChaincodeStubInterface, outputSlice interface{}, query string) error {
-	resultsIterator, err := stub.GetQueryResult(query)
+func FindAll(stub shim.ChaincodeStubInterface, outputSlice interface{}, query string, opts ...OptionFindAll) error {
+	resultsIterator, err := options(opts).selector()(stub, query)
 	if err != nil {
 		return ccErrors.NewBaseError(
 			ccErrors.ErrorGetState,
@@ -40,4 +41,38 @@ func FindAll(stub shim.ChaincodeStubInterface, outputSlice interface{}, query st
 		)
 	}
 	return nil
+}
+
+type options []OptionFindAll
+
+func (opts options) selector() selector {
+	for _, opt := range opts {
+		if opt.selector != nil {
+			return opt.selector
+		}
+	}
+	return defaultSelector
+}
+
+type OptionFindAll struct {
+	selector selector
+}
+
+type selector func(stub shim.ChaincodeStubInterface, query string) (shim.StateQueryIteratorInterface, error)
+
+func defaultSelector(stub shim.ChaincodeStubInterface, query string) (shim.StateQueryIteratorInterface, error) {
+	return stub.GetQueryResult(query)
+}
+
+func WithPagination(pageSize int32, bookmark string, addrOfPtr **peer.QueryResponseMetadata) OptionFindAll {
+	return OptionFindAll{selector: func(
+		stub shim.ChaincodeStubInterface,
+		query string,
+	) (shim.StateQueryIteratorInterface, error) {
+		it, md, err := stub.GetQueryResultWithPagination(query, pageSize, bookmark)
+		if err == nil && md != nil {
+			*addrOfPtr = md
+		}
+		return it, err
+	}}
 }
